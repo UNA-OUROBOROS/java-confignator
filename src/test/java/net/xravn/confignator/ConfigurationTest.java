8e96bd49a34f908503f64902920913a85c43d8af
Jonathan Guzmán <jonguz5@hotmail.com>
2022-04-15 12:32:06 -0600

    removed unused import

--- src/test/java/net/xravn/confignator/ConfigurationTest.java
+++ src/test/java/net/xravn/confignator/ConfigurationTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.security.Key;
-
 import org.junit.jupiter.api.Test;
 
 import net.xravn.confignator.exceptions.NodeNotFoundException;
